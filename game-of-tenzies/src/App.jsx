1b0ed129985acfdfd70f3bc6f3e4564cdee457e9
Sabare1 <sabaregirivasan@gmail.com>
2025-08-18 12:53:47 +0530

    added the dice components and initialised state for storing dice values

--- game-of-tenzies/src/App.jsx
+++ game-of-tenzies/src/App.jsx
@@ -1,5 +1,24 @@
+import './App.css'
+import { useState } from 'react'
+import Dice from './components/Dice'
+
 export default function App(){
+
+  const[dieNums, setDieNums] = useState(generateAllNewDice());
+
+  function generateAllNewDice(){
+    return new Array(10).fill(0).map(() => (Math.floor(Math.random()*6)+1))
+  }
+
+  const diceComp = dieNums.map((dieNum) => (
+    <Dice dieNum = {dieNum}/>
+  ))
+
   return(
-    <main></main>
+    <main className="container">
+      <section className='dice-container'>
+        {diceComp}
+      </section>
+    </main>
   )
 }
