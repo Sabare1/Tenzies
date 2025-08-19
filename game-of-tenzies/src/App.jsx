614771d5b50cc3540015bd1627ca8490466630cf
Sabare1 <sabaregirivasan@gmail.com>
2025-08-19 13:00:09 +0530

    added all the required states and features except the gamewon state

--- game-of-tenzies/src/App.jsx
+++ game-of-tenzies/src/App.jsx
@@ -1,24 +1,57 @@
 import './App.css'
 import { useState } from 'react'
 import Dice from './components/Dice'
+import {nanoid} from 'nanoid'
 
 export default function App(){
 
   const[dieNums, setDieNums] = useState(generateAllNewDice());
 
   function generateAllNewDice(){
-    return new Array(10).fill(0).map(() => (Math.floor(Math.random()*6)+1))
+    return new Array(10).fill(0).map(() => (
+      {
+        id: nanoid(),
+        randomNumber: Math.floor(Math.random()*6)+1,
+        isHeld: false
+      }
+    ))
+  }
+
+  function rollAll(){
+    setDieNums((prevDieNums) => (
+      prevDieNums.map((prevDieNum) => (
+        !prevDieNum.isHeld ? {
+          ...prevDieNum,
+          randomNumber: Math.floor(Math.random()*6)+1
+        }
+        : prevDieNum
+      ))
+    ));
+  }
+
+  function hold(id){
+    setDieNums((prevDieNums) => (
+      prevDieNums.map((dieNum) => (
+        {
+          ...dieNum,
+          isHeld: dieNum.id === id ? !dieNum.isHeld : dieNum.isHeld
+        }
+      ))
+    ))
   }
 
   const diceComp = dieNums.map((dieNum) => (
-    <Dice dieNum = {dieNum}/>
+    <Dice id={dieNum.id} key={dieNum.id} dieNum = {dieNum.randomNumber} isHeld={dieNum.isHeld} hold={hold}/>
   ))
 
   return(
     <main className="container">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <section className='dice-container'>
         {diceComp}
       </section>
+      <button className='roll-btn' onClick={rollAll}>Roll</button>
     </main>
   )
 }
--- game-of-tenzies/src/components/Dice.jsx
+++ game-of-tenzies/src/components/Dice.jsx
@@ -1,5 +1,6 @@
 export default function Dice(props){
+    const btnStyle = props.isHeld ? {backgroundColor: "#59E391"}: {backgroundColor:"white"}
     return(
-        <button className="dice">{props.dieNum}</button>
+        <button id={props.id} className="dice" style={btnStyle} onClick={() => (props.hold(props.id))}>{props.dieNum}</button>
     )
 }
