2561c98dce092fcfdb4ccdb68a2fc5e9d4920f8c
Sabare1 <sabaregirivasan@gmail.com>
2025-08-22 12:25:28 +0530

    100% completion of the tenzies

--- game-of-tenzies/src/App.jsx
+++ game-of-tenzies/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Dice from './components/Dice'
 import {nanoid} from 'nanoid'
+import {useWindowSize} from 'react-use'
+import { useRef } from 'react'
+import Confetti from 'react-confetti'
 
 export default function App(){
 
-  const[dieNums, setDieNums] = useState(generateAllNewDice());
+  const[dieNums, setDieNums] = useState(() => generateAllNewDice());
+  const button = useRef(null);
+  const gameWon = (
+    dieNums.every((dieNum) => dieNum.isHeld) &&
+    dieNums.every((dieNum) => dieNum.randomNumber === dieNums[0].randomNumber)
+  );
+  
+  useEffect(() => {
+    if(gameWon)
+      button.current.focus()
+  },[gameWon])
+  
 
   function generateAllNewDice(){
     return new Array(10).fill(0).map(() => (
@@ -18,16 +32,23 @@ export default function App(){
   }
 
   function rollAll(){
-    setDieNums((prevDieNums) => (
-      prevDieNums.map((prevDieNum) => (
-        !prevDieNum.isHeld ? {
-          ...prevDieNum,
-          randomNumber: Math.floor(Math.random()*6)+1
-        }
-        : prevDieNum
-      ))
-    ));
+    if(gameWon){
+      setDieNums(generateAllNewDice());
+    }
+    else{
+      setDieNums((prevDieNums) => (
+        prevDieNums.map((prevDieNum) => (
+          !prevDieNum.isHeld ? {
+            ...prevDieNum,
+            randomNumber: Math.floor(Math.random()*6)+1
+          }
+          : prevDieNum
+        ))
+      ));
+    }
   }
+
+  const {width, height} = useWindowSize();
 
   function hold(id){
     setDieNums((prevDieNums) => (
@@ -46,12 +67,13 @@ export default function App(){
 
   return(
     <main className="container">
+      {gameWon && <Confetti width={width} height = {height}/>}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <section className='dice-container'>
         {diceComp}
       </section>
-      <button className='roll-btn' onClick={rollAll}>Roll</button>
+      <button ref={button} className='roll-btn' onClick={rollAll}>{gameWon ? "New game" : "Roll"}</button>
     </main>
   )
 }
